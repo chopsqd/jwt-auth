06e835493e7913c7b5269bf17177f44ad6b51bd9
Michael Chop <007micle007@gmail.com>
2022-12-02 18:08:57 +0300

    Made the basic layout of the project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const App: FC = () => {
+    return (
+        <div>
+           123
+        </div>
+    );
+};
+
+export default App;
---
+++ server/controllers/user-controller.js
@@ -0,0 +1,51 @@
+class UserController {
+    async registration(req, res, next) {
+        try {
+
+        } catch(err) {
+
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+
+        } catch(err) {
+
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+
+        } catch(err) {
+
+        }
+    }
+
+    async activate(req, res, next) {
+        try {
+
+        } catch(err) {
+
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+
+        } catch(err) {
+
+        }
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            res.send('Its working!')
+        } catch(err) {
+
+        }
+    }
+}
+
+module.exports = new UserController()
