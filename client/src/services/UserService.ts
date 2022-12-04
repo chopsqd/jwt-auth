227b2fc35d8a8606ed70d8cbdfe7386ad9d8cdc1
Michael Chop <007micle007@gmail.com>
2022-12-04 16:45:09 +0300

    Added a couple of services for the client side

---
+++ client/src/services/UserService.ts
@@ -0,0 +1,9 @@
+import {IUser} from "../models/IUser";
+import {AxiosResponse} from "axios";
+import $api from "../http";
+
+export default class UserService {
+    static fetchUsers(): Promise<AxiosResponse<IUser[]>> {
+        return $api.get<IUser[]>('/users')
+    }
+}
